c3ec05d3076cc05ca723dc9a83b5f50f3e943db5
joregesosa <joregesosa@gmail.com>
2024-05-14 09:18:08 -0400

    main stayle changes

--- resources/js/Components/SideBar/index.jsx
+++ resources/js/Components/SideBar/index.jsx
@@ -8,7 +8,10 @@ export default function SideBar() {
     const width = isMouseOver ? 'w-64 px-8' : 'w-20 px-4';
 
     return (
-        <aside className={` min-h-[calc(100vh-73px)] h-full bg-${theme}-secondary m-1 mr-0 py-6 flex-shrink-0 ${width} transition-all duration-500 overflow-hidden rounded-md shadow`} onMouseOver={() => handleMouseOverSidebar(true)} onMouseOut={() => handleMouseOverSidebar(false)}>
+        <aside className={` min-h-screen h-full bg-${theme}-secondary  mr-0 py-6 flex-shrink-0 ${width} transition-all duration-500 overflow-hidden  shadow`} onMouseOver={() => handleMouseOverSidebar(true)} onMouseOut={() => handleMouseOverSidebar(false)}>
+            <picture className='w-full h-44 bg-gray-600'>
+                    <img src="" alt="" />
+            </picture>
 
             <Menu
                 isMouseOver={isMouseOver}
