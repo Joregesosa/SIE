796fe26efa94c0b494cd49a87623c099bbccb365
joregesosa <joregesosa@gmail.com>
2024-05-19 19:59:29 -0400

    Refactor sidebar menu component and add sections configuration

--- resources/js/hooks/useTable.jsx
+++ resources/js/hooks/useTable.jsx
@@ -85,7 +85,7 @@ export const useTable = (data) => {
     );
 
     const tableConfig = {
-        ref: dt, value: dataList, dataKey: 'id', paginator: true, rows: 5, rowsPerPageOptions: [5, 10, 25], paginatorTemplate: "FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown", currentPageReportTemplate: "Showing {first} to {last} of {totalRecords} usuarios", globalFilter: globalFilter, header: RenderSearch, scrollable: true, paginatorClassName: `bg-${theme}-primary text-${theme}-text rounded-b-md`
+        ref: dt, value: dataList, dataKey: 'id', paginator: true, rows: 5, rowsPerPageOptions: [5, 10, 25], paginatorTemplate: "FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown", currentPageReportTemplate: "Showing {first} to {last} of {totalRecords} usuarios", globalFilter: globalFilter, header: RenderSearch, scrollable: true, paginatorClassName: `bg-${theme}-secondary text-${theme}-text rounded-b-md`
     }
 
     return {
