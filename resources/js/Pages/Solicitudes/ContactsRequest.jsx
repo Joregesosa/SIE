362add37a2415aae3d6b2bad95712828b405f850
SirBeho <benjamin.tavarez.98@gmail.com>
2024-06-20 13:27:43 -0400

    Fix correos

--- resources/js/Pages/Solicitudes/ContactsRequest.jsx
+++ resources/js/Pages/Solicitudes/ContactsRequest.jsx
@@ -1,12 +1,12 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
-import React, { useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Toolbar } from 'primereact/toolbar';
-import DeleteAlert from '@/Components/Alerts/Delete.Alert';
-import { useTable } from '@/hooks/useTable';
-import { New } from '@/Components/Groups/New';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+import React, { useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Toolbar } from "primereact/toolbar";
+import DeleteAlert from "@/Components/Alerts/Delete.Alert";
+import { useTable } from "@/hooks/useTable";
+import { New } from "@/Components/Groups/New";
 
 export default function ContactsRequest({ auth, data, msj }) {
     const {
@@ -21,49 +21,77 @@ export default function ContactsRequest({ auth, data, msj }) {
         tableConfig,
         showNewDialog,
         setShowNewDialog,
-        RenderActionLinks
-    } = useTable(data)
+        RenderActionLinks,
+    } = useTable(data);
 
     useEffect(() => {
-        setDataList(data)
-        setAlert(msj)
-    }, [data, msj])
+        setDataList(data);
+        setAlert(msj);
+    }, [data, msj]);
 
     const RenderName = (rowData) => {
-        return rowData.first_name + ' ' + rowData.fLast_name
-    }
+        return rowData.first_name + " " + rowData.fLast_name;
+    };
 
     const requestStatus = (rowData) => {
         if (rowData.status === 1) {
-            return <span className="rounded-md bg-sky-500 text-jewel-text py-1 px-2">Pendiente</span>
+            return (
+                <span className="rounded-md bg-sky-500 text-jewel-text py-1 px-2">
+                    Pendiente
+                </span>
+            );
         }
 
         if (rowData.status === 2) {
-            return <span className="rounded-md bg-lime-500 text-jewel-text py-1 px-2">Aprobada</span>
+            return (
+                <span className="rounded-md bg-orange-500 text-jewel-text py-1 px-2">
+                    Aprobada
+                </span>
+            );
         }
 
         if (rowData.status === 3) {
-            return <span className="rounded-md bg-red-500 text-jewel-text py-1 px-2">Rechazada</span>
+            return (
+                <span className="rounded-md bg-green-500 text-jewel-text py-1 px-2">
+                    Completada
+                </span>
+            );
         }
-    }
+
+        if (rowData.status === 4) {
+            return (
+                <span className="rounded-md bg-red-500 text-jewel-text py-1 px-2">
+                    Rechazada
+                </span>
+            );
+        }
+    };
 
     const link = (rowData) => {
-             
-             return <Link href={route('inscription.create',{ contact: rowData.key, card: rowData.id_card })} className="cursor-pointer rounded-md bg-red-500 text-jewel-text py-1 px-2">
-                        Ir al Formulario -
-                    </Link>
-    }
-
+        if (rowData.status == 1) {
+            return <span>Pendiente de Aprobación</span>;
+        }
+        if (rowData.status == 2) {
+            return (
+                <Link
+                    href={route("inscription.create", {
+                        contact: rowData.key,
+                        card: rowData.id_card,
+                    })}
+                    className="cursor-pointer rounded-md bg-green-500 text-jewel-text py-1 px-2">
+                    Ir al Formulario -
+                </Link>
+            );
+        }
+    };
 
     const getDate = (rowData) => {
-        const date = new Date(rowData.created_at)
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return date.toLocaleDateString('es-ES', options)
-    }
+        const date = new Date(rowData.created_at);
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        return date.toLocaleDateString("es-ES", options);
+    };
 
-    const edit = (rowData) => {
-      
-    }
+    const edit = (rowData) => {};
 
     return (
         <AuthenticatedLayout
@@ -72,45 +100,63 @@ export default function ContactsRequest({ auth, data, msj }) {
         >
             <Head title="Lista de Solicitudes" />
 
-            <div className='h-[calc(100vh-120px)] rounded-b-md flex flex-col'>
+            <div className="h-[calc(100vh-120px)] rounded-b-md flex flex-col">
+                <Toolbar
+                    left={RenderLeftToolbar}
+                    right={() => RenderRightToolbar(dt)}
+                    className="py-2 rounded-none"
+                />
 
-                <Toolbar left={RenderLeftToolbar} right={() => RenderRightToolbar(dt)} className='py-2 rounded-none' />
+                <DataTable {...tableConfig}>
+                    <Column field="id" header="ID" sortable />
 
-                <DataTable  {...tableConfig}>
+                    <Column header="Nombre" sortable body={RenderName} />
 
-                    <Column field='id' header='ID' sortable />
+                    <Column field="email" header="Email" sortable />
 
-                    <Column  header='Nombre' sortable body={RenderName} />
+                    <Column
+                        header="Fecha de solicitud"
+                        sortable
+                        body={getDate}
+                    />
 
-                    <Column field='email' header='Email' sortable />
+                    <Column
+                        field="responseDate"
+                        header="Provisional-Matriculacion"
+                        sortable
+                        body={link}
+                    />
 
-                    <Column header='Fecha de solicitud' sortable body={getDate}/>
+                    <Column
+                        field="status"
+                        header="Estatus"
+                        sortable
+                        body={requestStatus}
+                    />
 
-                    <Column field='responseDate' header='Provisional-Matriculacion' sortable body={link} />
-
-                    <Column field='status' header='Estatus' sortable body={requestStatus} />
-
-                    <Column header="Acciones" body={(rowData) => RenderActionLinks(rowData , 'contact.show')} exportable={false} />
-
+                    <Column
+                        header="Acciones"
+                        body={(rowData) =>
+                            RenderActionLinks(rowData, "contact.show")
+                        }
+                        exportable={false}
+                    />
                 </DataTable>
-
             </div>
-    
+
             <New
                 showDialog={showNewDialog}
                 hideDialog={() => setShowNewDialog(false)}
             />
 
-
             <DeleteAlert
                 itemId={selectedItem.id}
                 value={selectedItem.email}
                 message={"el estudiante"}
-                endpoint=''
+                endpoint=""
                 showDialog={deleteItemDialog}
                 hideDialog={hideDeleteDialog}
             />
-
         </AuthenticatedLayout>
     );
 }
