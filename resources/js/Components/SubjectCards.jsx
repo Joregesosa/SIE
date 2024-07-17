1441d13c00aab2c0cc907535e009daa636750bc7
ChissgaN <coderchissgan@gmail.com>
2024-07-16 20:08:40 -0600

    scores frontend

---
+++ resources/js/Components/SubjectCards.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+
+export default function SubjectCards({ subjects, levelDescription, groupName, onSubjectClick, onBack }) {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-6">
+            {subjects.map((subjectScore) => (
+                <div key={subjectScore.subject.id} className="flex justify-center items-center">
+                    <Card title={subjectScore.subject.name} subTitle={`${groupName} - ${levelDescription}`} className="rounded-lg shadow-md text-center">
+                        <div className="flex justify-center">
+                            <Button onClick={() => onSubjectClick(subjectScore.subject)} className="bg-blue-500 w-[60%] rounded-md py-2 px-4  text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-center">
+                                Ver Calificaciones
+                            </Button>
+                        </div>
+                    </Card>
+                </div>
+            ))}
+        </div>
+    );
+}
